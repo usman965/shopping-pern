529c01c97f9989f62cef930db5a9ab4436e3b073
usman asghar <usmanasghar00000@gmail.com>
2026-04-09 19:50:53 +0500

    fix: added domain to vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['devusmanasghar.site', 'www.devusmanasghar.site'],
+    // If you want to allow everything (less secure but easier for testing):
+    // allowedHosts: true 
+  }
 })
